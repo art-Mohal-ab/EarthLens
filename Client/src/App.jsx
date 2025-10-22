bb6a0b8adcb6d345d31228137ca22342de76420c
Mohamed Farah <mohamedfayafarah@gmail.com>
2025-10-22 17:36:44 +0300

    Hero section added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,13 @@
  import React from "react"
  import Navbar from "./components/Navbar/Navbar"
+ import Hero from "./components/Hero/Hero"
  import Footer from "./components/Footer/Footer"
 
  const App = () => {
   return(
     <div>
       <Navbar/>
+      <Hero/>
       <Footer/>
     </div>
   )
