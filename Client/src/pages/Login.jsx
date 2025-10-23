15908eb2de36a959fcb25ead71cc5d279533fbd3
Mohamed Farah <mohamedfayafarah@gmail.com>
2025-10-23 07:38:16 +0300

    Login

--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -6,7 +6,7 @@ const Login = () => {
   return (
     <div className="login-container">
       <div className="login-box">
-        <h2>Welcome back!</h2>
+        <h2>Welcome Back To EarthLens</h2>
         <form>
           <label htmlFor="email">Email</label>
           <input
