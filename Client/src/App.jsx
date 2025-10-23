13806f7c82bd3c2b9ab03bc5a0d58336d23b5d7f
Claire Mbogo <claire.mbogo@student.moringaschool.com>
2025-10-24 01:20:54 +0300

    Add /about route to App.jsx routing

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Dashboard from "./Dashboard"
 import HomePage from "./HomePage"
+import About from "./About"
 
 const App = () => {
   return(
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   )
