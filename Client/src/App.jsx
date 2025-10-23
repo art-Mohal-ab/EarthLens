11a54ad8aa8b6bf993ce141bda248a4274429ec7
Mohamed Farah <mohamedfayafarah@gmail.com>
2025-10-23 07:36:06 +0300

    Login page added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,21 +1,36 @@
- import React from "react"
- import Navbar from "./components/Navbar/Navbar"
- import Hero from "./components/Hero/Hero"
- import Cards from "./components/Cards/Cards"
- import IssueCards from "./components/IssueCards/IssueCards"
- import Footer from "./components/Footer/Footer"
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import Cards from "./components/Cards/Cards";
+import IssueCards from "./components/IssueCards/IssueCards";
+import Footer from "./components/Footer/Footer";
+import Login from "./pages/Login";
 
+const App = () => {
+  return (
+    <Router>
+      <div className="app">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route 
+              path="/" 
+              element={
+                <>
+                  <Hero />
+                  <Cards />
+                  <IssueCards />
+                </>
+              } 
+            />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+};
 
- const App = () => {
-  return(
-    <div>
-      <Navbar/>
-      <Hero/>
-      <Footer/>
-      <Cards/>
-      <IssueCards />
-    </div>
-  )
- }
-
-export default App
+export default App;
