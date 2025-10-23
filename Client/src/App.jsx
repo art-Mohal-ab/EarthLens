fd5b7ca8ee5102301d9548452dbad01a3591fdf9
Mohamed Farah <mohamedfayafarah@gmail.com>
2025-10-23 06:31:30 +0300

    Footer fixed

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,21 +1,22 @@
- import React from "react"
- import Navbar from "./components/Navbar/Navbar"
- import Hero from "./components/Hero/Hero"
- import Cards from "./components/Cards/Cards"
- import IssueCards from "./components/IssueCards/IssueCards"
- import Footer from "./components/Footer/Footer"
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import Cards from "./components/Cards/Cards";
+import IssueCards from "./components/IssueCards/IssueCards";
+import Footer from "./components/Footer/Footer";
 
-
- const App = () => {
-  return(
-    <div>
-      <Navbar/>
-      <Hero/>
-      <Footer/>
-      <Cards/>
-      <IssueCards />
+const App = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <main>
+        <Hero />
+        <Cards />
+        <IssueCards/>
+      </main>
+      <Footer />
     </div>
-  )
- }
+  );
+};
 
-export default App
+export default App;
