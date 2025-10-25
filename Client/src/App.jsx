c47fbdfc233e48832a060778f9369d4208916102
Claire Mbogo <claire.mbogo@student.moringaschool.com>
2025-10-25 15:27:56 +0300

    Revert App.jsx to original routing structure without Navbar, Hero, Footer

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,15 +1,18 @@
 import React from "react"
- import Navbar from "./components/Navbar/Navbar"
- import Hero from "./components/Hero/Hero"
- import Footer from "./components/Footer/Footer"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Dashboard from "./Dashboard"
+import HomePage from "./HomePage"
+import About from "./About"
 
 const App = () => {
   return(
-    <div>
-      <Navbar/>
-      <Hero/>
-      <Footer/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   )
 }
 
