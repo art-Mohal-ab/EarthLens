5fe92e1dec388f95744aa529ec8b4fd42df28125
Claire Mbogo <claire.mbogo@student.moringaschool.com>
2025-10-25 15:31:33 +0300

    Add routing to App.jsx for login, join, dashboard, about pages with Navbar and Footer hidden on certain pages

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import LandingPage from './pages/LandingPage';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Dashboard from './Dashboard';
+import About from './About';
+
+const AppContent = () => {
+  const location = useLocation();
+  const hideNavFooter = ['/login', '/join', '/dashboard'].includes(location.pathname);
+
+  return (
+    <div>
+      {!hideNavFooter && <Navbar />}
+      <main>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/join" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+      {!hideNavFooter && <Footer />}
+    </div>
+  );
+};
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <LandingPage />
-      <Footer />
-    </div>
+    <Router>
+      <AppContent />
+    </Router>
   );
 };
 
