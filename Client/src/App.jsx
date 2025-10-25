cb5cfc6078eff5e5ec029df6ea873b28e15080c4
Claire Mbogo <claire.mbogo@student.moringaschool.com>
2025-10-25 15:25:53 +0300

    Revert App.jsx to original structure with Navbar, Hero, Footer

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,43 +1,16 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
-import Login from "./pages/Login";
-import LandingPage from './pages/LandingPage';
-import Signup from "./pages/Signup";
-import Dashboard from "./Dashboard";
-import HomePage from "./HomePage";
-import About from "./About";
-
-const AppContent = () => {
-  const location = useLocation();
-  const hideNavFooter = ['/login', '/join', '/dashboard'].includes(location.pathname);
-  const hideFooter = ['/login', '/join', '/dashboard'].includes(location.pathname);
-
-  return (
-    <div className="app">
-      {!hideNavFooter && <Navbar />}
-      <main>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/join" element={<Signup />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </main>
-      {!hideFooter && <Footer />}
-    </div>
-  );
-};
+import React from "react"
+ import Navbar from "./components/Navbar/Navbar"
+ import Hero from "./components/Hero/Hero"
+ import Footer from "./components/Footer/Footer"
 
 const App = () => {
-  return (
-    <Router>
-      <AppContent />
-    </Router>
-  );
-};
+  return(
+    <div>
+      <Navbar/>
+      <Hero/>
+      <Footer/>
+    </div>
+  )
+}
 
-export default App;
+export default App
