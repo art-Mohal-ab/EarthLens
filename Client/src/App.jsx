9ce6a738942a54cdc10fe93b4fb3d2c642bbee2d
Claire Mbogo <claire.mbogo@student.moringaschool.com>
2025-10-25 15:30:16 +0300

    Add Navbar and Footer to App.jsx around LandingPage

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 import LandingPage from './pages/LandingPage';
 
 const App = () => {
   return (
-    <LandingPage />
+    <div>
+      <Navbar />
+      <LandingPage />
+      <Footer />
+    </div>
   );
 };
 
