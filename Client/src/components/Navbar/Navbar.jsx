2692da19f90e37da7d07fa98b716b2f79277c66a
Claire Mbogo <claire.mbogo@student.moringaschool.com>
2025-10-27 03:09:26 +0300

    update navbar and footer

--- Client/src/components/Navbar/Navbar.jsx
+++ Client/src/components/Navbar/Navbar.jsx
@@ -1,47 +1,17 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom';
 import './Navbar.css'
 
 
 const Navbar = () => {
-    const navigate = useNavigate();
-
-    const handleDashboard = () => {
-        navigate("/dashboard");
-    };
-
-    const handleReport = () => {
-        navigate("/dashboard");
-    };
-
-    const handleMyReports = () => {
-        navigate("/my-reports");
-    };
-
-    const handleGreenAction = () => {
-        navigate("/dashboard");
-    };
-
-    const handleProfile = () => {
-        navigate("/profile");
-    };
-
-    const handleSignOut = () => {
-        localStorage.removeItem('token');
-        navigate("/");
-    };
-
-    return (
+    return ( 
     <header className="header">
         <a href="/" className='logo'>Earth<br/>lens</a>
         <img src="/logo.png" alt="Earthlens Logo" className="logo-img" />
         <nav className='navbar'>
-            <button className="nav-button" onClick={handleDashboard}>Dashboard</button>
-            <button className="nav-button" onClick={handleReport}>Report</button>
-            <button className="nav-button" onClick={handleMyReports}>My Reports</button>
-            <button className="nav-button" onClick={handleGreenAction}>Green Action</button>
-            <button className="nav-button" onClick={handleProfile}>Profile</button>
-            <button className="nav-button sign-out-button" onClick={handleSignOut}>Sign out</button>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/">Contact</a>
+            <a href="/join">Join Now</a>
         </nav>
     </header>
    )
