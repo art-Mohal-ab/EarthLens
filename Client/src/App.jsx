591e57829cc4aaf3053d3805de5ab1c5767d4313
Claire Mbogo <claire.mbogo@student.moringaschool.com>
2025-10-27 21:22:16 +0300

    Fix import path for About component in App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Dashboard from "./pages/Dashboard"
 import HomePage from "./pages/HomePage"
-import About from "./About"
+import About from "./pages/About"
 import Profile from "./pages/Profile"
 import MyReportspage from "./pages/MyReportspage"
 
