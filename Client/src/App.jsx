2abb95cc250db24af39417cf449bda582214a6fe
Mercy <kiqsmercy@gmail.com>
2025-10-27 22:10:49 +0300

    Refactor App component to implement routing with react-router-dom

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,15 +1,23 @@
 import React from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar/Navbar"
 import Footer from "./components/Footer/Footer"
 import Report from "./pages/Report.jsx"
+import GreenActions from "./pages/GreenActions.jsx"
 
 const App = () => {
   return(
-    <div>
-      <Navbar/>
-      <Report/>
-      <Footer/>
-    </div>
+    <Router>
+      <div>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Report/>} />
+          <Route path="/report" element={<Report/>} />
+          <Route path="/green-actions" element={<GreenActions/>} />
+        </Routes>
+        <Footer/>
+      </div>
+    </Router>
   )
 }
 
