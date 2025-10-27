2a5c22f0d78a31cd9abcc5b1cb260c9d93578a52
Mohamed Farah <mohamedfayafarah@gmail.com>
2025-10-27 05:27:58 +0300

    Contact route added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,7 +5,7 @@ import Footer from './components/Footer/Footer';
 import LandingPage from './pages/LandingPage';
 import Signup from "./pages/Signup";
 import Login from './pages/Login'; 
-
+import Contact from './pages/Contact';
 
 const noFooterRoutes = ["/login"];
 
@@ -21,6 +21,7 @@ const AppContent = () => {
             <Route path="/" element={<LandingPage />} />
             <Route path="/login" element={<Login />} />
             <Route path="/Signup" element={<Signup />} />
+            <Route path="/Contact" element={<Contact />}/>
 
           </Routes>
       </main>
