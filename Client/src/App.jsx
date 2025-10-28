11f1b5fd511e846a7bba938c101a14320ff328f7
Claire Mbogo <claire.mbogo@student.moringaschool.com>
2025-10-29 00:49:28 +0300

    initial commit

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,16 +1,26 @@
 import React from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Dashboard from "./Dashboard"
-import HomePage from "./HomePage"
-import About from "./About"
+import Dashboard from "./pages/Dashboard.jsx"
+import HomePage from "./pages/HomePage.jsx"
+import About from "./pages/About.jsx"
+import LandingPage from "./pages/LandingPage.jsx"
+import Signup from "./pages/Signup.jsx"
+import Login from "./pages/Login.jsx"
+import MyReportspage from "./pages/MyReportspage.jsx"
+import Profile from "./pages/Profile.jsx"
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/about" element={<About />} />
+        <Route path="/join" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/my-reports" element={<MyReportspage />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   );
--- Client/src/components/Navbar/Navbar.jsx
+++ Client/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Navbar.css'
 
 
 const Navbar = () => {
-    return ( 
+    return (
     <header className="header">
         <Link to="/" className='logo'>Earth<br/>lens</Link>
         <img src="/logo.png" alt="Earthlens Logo" className="logo-img" />
         <nav className='navbar'>
-            <a href="/">Home</a>
-            <a href="/">Features</a>
-            <a href="/about">About Us</a>
-            <a href="/">Contact Us</a>
-            <a href="/">Join Now</a>
+            <Link to="/">Home</Link>
+            <Link to="/">Features</Link>
+            <Link to="/about">About Us</Link>
+            <Link to="/">Contact Us</Link>
+            <Link to="/join">Join Now</Link>
         </nav>
     </header>
    )
