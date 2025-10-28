b333030e5549993af4823607b538cd9463a234c3
Claire Mbogo <claire.mbogo@student.moringaschool.com>
2025-10-28 14:49:18 +0300

    intial commit

--- Client/src/components/ViewReportModal.jsx
+++ Client/src/components/ViewReportModal.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 
 const ViewReportModal = ({ report, isOpen, onClose }) => {
-  // Generate contextual comments based on report category
   const getContextualComments = (report) => {
     const baseComments = {
       "Waste Management": [
