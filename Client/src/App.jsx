fefb6e8890d0390e92993c93c5db5e83408aee85
Claire Mbogo <claire.mbogo@student.moringaschool.com>
2025-10-29 01:33:52 +0300

    remove homepage

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Dashboard from "./pages/Dashboard.jsx"
-import HomePage from "./pages/HomePage.jsx"
 import About from "./pages/About.jsx"
 import LandingPage from "./pages/LandingPage.jsx"
 import Signup from "./pages/Signup.jsx"
@@ -14,7 +13,6 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/home" element={<HomePage />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/about" element={<About />} />
         <Route path="/join" element={<Signup />} />
