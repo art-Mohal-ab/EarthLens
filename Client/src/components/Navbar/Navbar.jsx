325c97ee6057d3feeebe510a278b0fff809503e2
Mohamed Farah <mohamedfayafarah@gmail.com>
2025-10-29 13:33:43 +0300

    navbar fixed

--- Client/src/components/Navbar/Navbar.jsx
+++ Client/src/components/Navbar/Navbar.jsx
@@ -1,19 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import './Navbar.css'
 
 
 const Navbar = () => {
-    return (
+    return ( 
     <header className="header">
-        <Link to="/" className='logo'>Earth<br/>lens</Link>
+        <a href="/" className='logo'>Earth<br/>lens</a>
         <img src="/logo.png" alt="Earthlens Logo" className="logo-img" />
         <nav className='navbar'>
-            <Link to="/">Home</Link>
-            <Link to="/">Features</Link>
-            <Link to="/about">About Us</Link>
-            <Link to="/">Contact Us</Link>
-            <Link to="/join">Join Now</Link>
+            <a href="/">Home</a>
+            <a href="/">About</a>
+            <a href="/">Contact</a>
         </nav>
     </header>
    )
